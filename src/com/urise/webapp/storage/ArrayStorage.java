48c91ef6126c1947d83b111b3014cff996f2549f
Vlad_Milas <12milash12@gmail.com>
2024-03-30 15:01:46 +0300

    Implements methods save, get, delete, clear, getAll, size

---
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -0,0 +1,59 @@
+package com.urise.webapp.storage;
+
+import com.urise.webapp.model.Resume;
+
+import java.util.Arrays;
+
+/**
+ * Array based storage for Resumes
+ */
+public class ArrayStorage {
+    private Resume[] storage = new Resume[10000];
+    private int size = 0;
+
+    public void clear() {
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
+    }
+
+    public void save(Resume resume) {
+        if (size < storage.length) {
+            storage[size] = resume;
+            size++;
+        } else {
+            System.out.println("There is no more place to store your resume");
+        }
+    }
+
+    public Resume get(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].getUuid().equals(uuid)) {
+                return storage[i];
+            }
+        }
+        System.out.println("The specified resume could not be found");
+        return null;
+    }
+
+    public void delete(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].getUuid().equals(uuid)) {
+                size--;
+                storage[i] = storage[size];
+                storage[size] = null;
+                break;
+            }
+        }
+    }
+
+    /**
+     * @return array, contains only Resumes in storage (without null)
+     */
+    public Resume[] getAll() {
+        return Arrays.copyOf(storage, size);
+    }
+
+    public int size() {
+        return size;
+    }
+}
