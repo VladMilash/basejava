c88d82fcc71cd6ef6cb81ca687aa9117839101a2
Vlad_Milas <12milash12@gmail.com>
2024-04-07 15:19:45 +0300

    Implements HW_3

--- src/com/urise/webapp/storage/SortedArrayStorage.java
+++ src/com/urise/webapp/storage/SortedArrayStorage.java
@@ -9,8 +9,8 @@ public class SortedArrayStorage extends AbstractArrayStorage {
     @Override
     protected void saveElement(Resume resume) {
         int insertionPoint = -(Arrays.binarySearch(storage, 0, size, resume)) - 1;
-        if (insertionPoint < size - 1) {
-            System.arraycopy(storage,insertionPoint, storage, insertionPoint + 1,size - 1);
+        if (insertionPoint < size) {
+            System.arraycopy(storage,insertionPoint, storage, insertionPoint + 1,size - insertionPoint);
             storage[insertionPoint] = resume;
             incrementSize();
         } else {
@@ -23,7 +23,7 @@ public class SortedArrayStorage extends AbstractArrayStorage {
     @Override
     protected void deleteElement(int index) {
         if (index < size -1) {
-            System.arraycopy(storage,index + 1, storage, index,size - 1);
+            System.arraycopy(storage,index + 1, storage, index,size - index - 1);
             clearElement(storage, size - 1);
         } else {
             clearElement(storage, index);
