96b28ffa437817cfb6b55db7d817c624bbe3c077
Vlad_Milas <12milash12@gmail.com>
2024-05-03 16:03:16 +0300

    Implements HW_6

---
+++ test/com/urise/webapp/storage/MapStorageResumeTest.java
@@ -0,0 +1,9 @@
+package com.urise.webapp.storage;
+
+import static org.junit.Assert.*;
+
+public class MapStorageResumeTest extends AbstractArrayStorageTest {
+    public MapStorageResumeTest() {
+        super(new MapStorageResume());
+    }
+}
