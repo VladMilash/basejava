0d86fcdcf1124defb1545a1a88e1fa3311e55691
Vlad_Milas <12milash12@gmail.com>
2024-05-16 14:47:18 +0300

    Implements HW_8

--- src/com/urise/webapp/model/Company.java
+++ src/com/urise/webapp/model/Company.java
@@ -7,7 +7,6 @@ public class Company {
     private final String title;
     private final String webSite;
     private final List<Period> periods;
-    private List<Period> periodsSecond;
 
     public Company(String title, String webSite, List<Period> periods) {
         Objects.requireNonNull(title, "title must not be null");
@@ -18,11 +17,6 @@ public class Company {
         this.periods = periods;
     }
 
-    public Company(String title, String webSite, List<Period> periods, List<Period> periodsSecond) {
-        this(title, webSite, periods);
-        Objects.requireNonNull(periodsSecond, "periodsSecond must not be null");
-        this.periodsSecond = periodsSecond;
-    }
 
     public String getTitle() {
         return title;
@@ -36,21 +30,17 @@ public class Company {
         return periods;
     }
 
-    public List<Period> getPeriodsSecond() {
-        return periodsSecond;
-    }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Company company = (Company) o;
-        return Objects.equals(title, company.title) && Objects.equals(webSite, company.webSite) && Objects.equals(periods, company.periods) && Objects.equals(periodsSecond, company.periodsSecond);
+        return Objects.equals(title, company.title) && Objects.equals(webSite, company.webSite) && Objects.equals(periods, company.periods);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(title, webSite, periods, periodsSecond);
+        return Objects.hash(title, webSite, periods);
     }
 
     @Override
@@ -59,7 +49,6 @@ public class Company {
                 "title='" + title + '\'' +
                 ", webSite='" + webSite + '\'' +
                 ", periods=" + periods +
-                ", periodsSecond=" + periodsSecond +
                 '}';
     }
 }
