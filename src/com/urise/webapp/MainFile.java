84796b1a259d418e71281662b924ca7206f7941f
Vlad_Milas <12milash12@gmail.com>
2024-05-22 12:32:04 +0300

    Implements HW_9

--- src/com/urise/webapp/MainFile.java
+++ src/com/urise/webapp/MainFile.java
@@ -5,17 +5,19 @@ import java.io.File;
 public class MainFile {
     public static void main(String[] args) {
         File file = new File("./src/com/urise/webapp");
-        printDirectory(file);
+        printDirectory(file, 0);
     }
 
-    public static void printDirectory(File file) {
+    public static void printDirectory(File file, int level) {
         File[] files = file.listFiles();
         if (files != null) {
             for (File value : files) {
-                if (value.isDirectory()) {
-                    printDirectory(value);
-                } else {
-                    System.out.println(value);
+                String indent = " ".repeat(level * 3);
+                if (value.isFile()) {
+                    System.out.println(indent + "File: " + value.getName());
+                } else if (value.isDirectory()) {
+                    System.out.println(indent + "Directory: " + value.getName());
+                    printDirectory(value, level + 1);
                 }
             }
         }
