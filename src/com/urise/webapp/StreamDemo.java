9bed46ef6a641ec3224af46a75468c412b5cdbf0
Vlad_Milas <12milash12@gmail.com>
2024-06-03 16:00:31 +0300

    Implements HW_12

---
+++ src/com/urise/webapp/StreamDemo.java
@@ -0,0 +1,29 @@
+package com.urise.webapp;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamDemo {
+    public static void main(String[] args) {
+        int [] values = {5,2,1,3,3,3};
+        System.out.println(minValue(values));
+
+        List<Integer> integers = Arrays.asList(1, 1, 1, 2, 2);
+        List<Integer> result = oddOrEven(integers);
+        System.out.println(result);
+    }
+
+        public static int minValue(int[] values) {
+            return Arrays.stream(values)
+                    .distinct()
+                    .sorted()
+                    .reduce(0,(a,b) -> a * 10 + b);
+        }
+
+        public static List<Integer> oddOrEven(List<Integer> integers) {
+            return integers.stream()
+                    .collect(Collectors.partitioningBy(i -> i % 2 != 0))
+                    .get(integers.stream().filter(i -> i % 2 != 0).count() % 2 == 0);
+        }
+}
