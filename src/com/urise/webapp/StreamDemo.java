e21c41654ff7d12afe7f466f82d8989c1a0d53b2
Vlad_Milas <12milash12@gmail.com>
2024-06-04 09:08:45 +0300

    Implements HW_12

--- src/com/urise/webapp/StreamDemo.java
+++ src/com/urise/webapp/StreamDemo.java
@@ -6,23 +6,23 @@ import java.util.stream.Collectors;
 
 public class StreamDemo {
     public static void main(String[] args) {
-        int [] values = {5,2,1,3,3,3};
+        int[] values = {5, 2, 1, 3, 3, 3};
         System.out.println(minValue(values));
 
         List<Integer> integers = Arrays.asList(1, 1, 1, 2, 2);
         System.out.println(oddOrEven(integers));
     }
 
-        public static int minValue(int[] values) {
-            return Arrays.stream(values)
-                    .distinct()
-                    .sorted()
-                    .reduce(0,(a,b) -> a * 10 + b);
-        }
+    public static int minValue(int[] values) {
+        return Arrays.stream(values)
+                .distinct()
+                .sorted()
+                .reduce(0, (a, b) -> a * 10 + b);
+    }
 
-        public static List<Integer> oddOrEven(List<Integer> integers) {
-            return integers.stream()
-                    .collect(Collectors.partitioningBy(i -> i % 2 != 0))
-                    .get(integers.stream().filter(i -> i % 2 != 0).count() % 2 == 0);
-        }
+    public static List<Integer> oddOrEven(List<Integer> integers) {
+        return integers.stream()
+                .collect(Collectors.partitioningBy(i -> i % 2 != 0))
+                .get(integers.stream().filter(i -> i % 2 != 0).count() % 2 == 0);
+    }
 }
